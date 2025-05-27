6f8b4ea418b0ab858ca9a5dbd56c591f6794ff09
TomasCapocasa <tomascapocasa@hotmail.com>
2025-05-26 21:43:16 -0300

    se agrega la condicion que no tenga reservas para eliminar

--- src/controllers/maquinaController.js
+++ src/controllers/maquinaController.js
@@ -169,6 +169,31 @@ const listarMaquinas = async (req, res) => {
   }
 };
 
-module.exports = {
-  listarMaquinas,
-};
+const agregarMaquina = async (req, res) => {
+    try {
+        const nuevaMaquina = await Maquina.create(req.body);
+        res.status(201).json(nuevaMaquina);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+const eliminarMaquina = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const maquina = await Maquina.findByPk(id);
+        
+        if (!maquina) {
+            return res.status(404).json({ error: 'Máquina no encontrada' });
+        }
+        if (maquina.reservas.length > 0) {
+            return res.status(400).json({ error: 'No se puede eliminar una máquina con reservas activas' });
+        }
+        await maquina.destroy();
+        res.status(204).end();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = {listarMaquinas, agregarMaquina, eliminarMaquina};
